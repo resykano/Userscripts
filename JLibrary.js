2380604c886c33f3ebdd7ba0d984fef1a78a8bb1
reseano <>
2026-01-20 01:15:04 +0100

    add delay to function execution

--- JLibrary.js
+++ JLibrary.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           JAVLibrary Improvements
 // @description    Many improvements mainly in details view of a video: video thumbnails below cover (deactivatable through Configuration in the browser extension menu), easier collect of Google Drive and Rapidgator links for JDownloader (hotkey < or \), save/show favorite actresses (since script installation), recherche links for actresses, auto reload on Cloudflare rate limit, save cover with actress names just by clicking, advertising photos in full size, remove redirects, layout improvements
-// @version        20260119
+// @version        20260120
 // @author         resykano
 // @icon           https://www.javlibrary.com/favicon.ico
 // @match          *://*.javlibrary.com/*
@@ -2771,7 +2771,9 @@ function main() {
                 addVideoThumbnails();
             };
 
-            executeFunctions();
+            setTimeout(() => {
+                executeFunctions();
+            }, 100);
         }
     }
 }
